715035bf5965e972be83f21e75a5a5cc568eb093
Ken <weiqh@pvc123.com>
2019-04-30 10:19:58 +0800

    add:新增css/style-loader实现样式热更新，使用tree shaking压缩

--- webpack4/src/index.js
+++ webpack4/src/index.js
@@ -1,20 +1,19 @@
-import _ from 'lodash'
 import printMe from './print.js';
-
+import './styles.css';
+import { cube } from './math.js';
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-  element.innerHTML = _.join(['Hello', 'ken'], ' ');
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe
-  element.appendChild(btn)
+  var element = document.createElement('pre');
+  element.innerHTML = [
+    'hello ken',
+    '5 cubed is equal to' + cube(5)
+  ].join('\n\n');
   return element;
 }
 document.body.appendChild(component());
 
-if(module.hot) {
-  module.hot.accept('./print.js', function () { 
-    console.log('Accepting the updated printMe module')
-    printMe()
-   })
-}
+// if(module.hot) {
+//   module.hot.accept('./print.js', function () { 
+//     console.log('Accepting the updated printMe module')
+//     printMe()
+//    })
+// }
